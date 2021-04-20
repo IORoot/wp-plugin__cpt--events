619288f9ed4234ae39aae4f4ebe6ee2051946e0f
Andy Pearson <andy@londonparkour.com>
2021-04-20 13:04:29 +0100

    rename

--- src/acf/andyp_plugin_register.php
+++ src/acf/andyp_plugin_register.php
@@ -2,7 +2,7 @@
 
 add_action( 'plugins_loaded', function() {
     do_action('register_andyp_plugin', [
-        'title'     => 'Labs - CPT: Event ',
+        'title'     => 'CPT: Event ',
         'icon'      => 'run-fast',
         'color'     => '#38EF7D',
         'path'      => __FILE__,
